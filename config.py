37fdd067476b6e6ca5c35cc6741a46dc955fdbbc
Kijiiin <82979633+Kijiiin@users.noreply.github.com>
2026-08-28 15:09:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@
 #   streamingcommunity.tv
 # ============================================================
 
-STREAMING_DOMAIN = "streamingcommunityz.studio"
+STREAMING_DOMAIN = "streamingcommunityz.style"
 
 # URL di base
 BASE_URL = f"https://{STREAMING_DOMAIN}"
